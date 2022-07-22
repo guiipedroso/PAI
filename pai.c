01d897d612ecac6b6bc63c0c5d8118b0da051fe4
Pedroso <guilhermepedrosogg@gmail.com>
2022-07-22 01:39:53 -0300

    PAI library

---
+++ pai.c
@@ -0,0 +1,14 @@
+#include "pai.h"
+
+void
+onset()
+  {
+  printf(" ____       _\n");
+  printf("|  _ \\ __ _(_)\n");
+  printf("| |_) / _` | |\n");
+  printf("|  __/ (_| | |\n");
+  printf("|_|   \\__,_|_|\n");
+  printf("\n");
+  printf("Programming Artificial Intelligence");
+  printf("\n\n");
+  }
